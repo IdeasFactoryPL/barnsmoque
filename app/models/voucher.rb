17596ade92ecddd2b21dd0a93ed789a9e7352c09
Rafał Petryka <rafalpetryka@gmail.com>
2015-10-28 11:05:12 +0100

    Added callback, which saves code downcase

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,4 +1,5 @@
 class Voucher < ActiveRecord::Base
 	include FindUser
 	validates_length_of :code, minimum: 8, maximium: 8, allow_blank: false
+  before_save { |voucher| voucher.code = voucher.code.downcase }
 end
